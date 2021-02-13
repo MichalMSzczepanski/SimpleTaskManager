f88f3c5115814b568a6a09a02a4f286e453c1012
Mike <michal.m.szczepanski>
2021-02-13 15:40:15 +0100

    create delete feature

--- src/main/java/com/coderslab/Main.java
+++ src/main/java/com/coderslab/Main.java
@@ -1,6 +1,9 @@
 package com.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,6 +32,8 @@ public class Main {
 
         try(Scanner dataSourceInput = new Scanner(dataSource)){
 
+            // COUNT THE NUMBER OF TASKS AT THE MOMENT
+
             int taskAmount = 0;
 
             while(dataSourceInput.hasNextLine()){
@@ -38,7 +43,8 @@ public class Main {
 
             System.out.println("(the number of registered tasks is: " + taskAmount + ")");
 
-            // TRANSFER STRINGS, VERSE BY VERSE INTO TABLE OF STRINGS AFTER SPLITING THEM INTO SEPARATE STRINGS
+
+            // TRANSFER STRINGS, VERSE BY VERSE INTO TABLE OF STRINGS AFTER SPLITTING THEM INTO SEPARATE STRINGS
 
             String[][] arrayOfTasks = new String[taskAmount][3];
 
@@ -50,7 +56,7 @@ public class Main {
 
                 for (int i = 0; i < arrayOfTasks.length; i++) {
                     String taskLine = arrayCreationScanner.nextLine();
-                    String[] taskLineArray = taskLine.split(",");
+                    String[] taskLineArray = taskLine.split(","); // french 2020 true
                     for (int j = 0; j < arrayOfTasks[i].length; j++) {
                         arrayOfTasks[i][j] = taskLineArray[j];
 //                        System.out.println(arrayOfTasks[i][j]); // CHECKING IF THIS WORKS
@@ -60,8 +66,6 @@ public class Main {
 
             }
 
-
-
             // CHOSE THE ACTION
 
             Scanner userInput = new Scanner(System.in);
@@ -70,23 +74,90 @@ public class Main {
 
                 String choice = userInput.nextLine();
 
-                switch (choice){
+                switch (choice) {
                     case "add":
-                        System.out.println("chose add");
+
+                        arrayOfTasks = Arrays.copyOf(arrayOfTasks, arrayOfTasks.length + 1);
+
+//                       // PRINT ALL TASKS TO VERIFY IF NEW LINE FOR NEW TASK ADDED
+//
+//                        for (int i = 0; i < arrayOfTasks.length; i++) {
+//                            for (int j = 0; j < arrayOfTasks[i].length; j++) {
+//                                System.out.print(arrayOfTasks[i][j] + " ");
+//                            }
+//                            System.out.println();
+//                        }
+
+                        String[] addedTask = new String[3];
+
+                        // description
+                        System.out.println("Please add task description");
+                        addedTask[0] = userInput.nextLine();
+                        // due date
+                        System.out.println("Please add task due date");
+                        addedTask[1] = userInput.nextLine();
+                        // true/ false
+                        System.out.println("Please add task priority");
+                        addedTask[2] = userInput.nextLine();
+
+                        arrayOfTasks[arrayOfTasks.length -1 ] = addedTask;
+
+                        System.out.println("your added task is: " + Arrays.toString(arrayOfTasks[arrayOfTasks.length -1])); // validate if task was added
+
+//                         PRINT ALL TASKS TO VERIFY IF NEW LINE FOR NEW TASK ADDED
+
+//                        for (int i = 0; i < arrayOfTasks.length; i++) {
+//                            for (int j = 0; j < arrayOfTasks[i].length; j++) {
+//                                System.out.print(arrayOfTasks[i][j] + " ");
+//                            }
+//                            System.out.println();
+//                        }
+
                         break;
+
                     case "remove":
-                        System.out.println("chose remove");
-                        break;
+                        System.out.println("Please select tak to remove");
+                        Scanner taskToBeRemoved = new Scanner(System.in);
+                        while(taskToBeRemoved.hasNextLine()) {
+                            while (!taskToBeRemoved.hasNextInt()) {
+                                taskToBeRemoved.next();
+                                System.out.println("provided value is not a number, try again");
+                                break;
+                            }
+
+                            int taskToRemove = taskToBeRemoved.nextInt();
+
+                            if (taskToRemove >= 0 && taskToRemove < arrayOfTasks.length) {
+                                arrayOfTasks = ArrayUtils.remove(arrayOfTasks, taskToRemove);
+                                System.out.println("removed task number " + taskToRemove);
+                                break;
+                            } else {
+                                System.out.println("provided value is below zero or reaches beyond the array, try again");
+                            }
+                        }
+                            break;
+
                     case "list":
-                        listTasks();
+
+                        for (int i = 0; i < arrayOfTasks.length; i++) {
+                            System.out.print(i + " : ");
+                            for (int j = 0; j < arrayOfTasks[i].length; j++) {
+                                System.out.print(arrayOfTasks[i][j] + " ");
+                            }
+                            System.out.println();
+                        }
                 }
 
-                if(choice.equals("quit")){
+                if (choice.equals("quit")) {
+
+
+
                     System.out.println();
-                    System.out.println("bye!");
+                    System.out.println("any changes saved, bye!");
                     System.out.println();
                     break;
                 }
+            }
 
                 System.out.println();
                 System.out.println("Please select an option");
@@ -94,7 +165,7 @@ public class Main {
                     System.out.println(optionsString);
                 }
 
-            }
+
 
         } catch (IOException e) {
             System.out.println("issue with file");
@@ -102,16 +173,31 @@ public class Main {
 
     }
 
+
+    // METHOD LISTING TASKS
+
     public static void listTasks(){
-        Path dataSource = Paths.get("/Users/mike/Dropbox/01podstawyJava/warsztat/05_attachment_Zasoby do projektu.pl/tasks.csv");
-        try(Scanner choiceListTasks = new Scanner(dataSource)) {
-            while (choiceListTasks.hasNextLine()) {
-                String task = choiceListTasks.nextLine();
-                System.out.println(task);
-            }
-        } catch (IOException e){
-            System.out.println("file not found");
-        }
+        // PRINTING TASKS FROM CSV FILE
+
+//        Path dataSource = Paths.get("/Users/mike/Dropbox/01podstawyJava/warsztat/05_attachment_Zasoby do projektu.pl/tasks.csv");
+//        try(Scanner choiceListTasks = new Scanner(dataSource)) {
+//            int taskCounter = 1;
+//            while (choiceListTasks.hasNextLine()) {
+//                String task = choiceListTasks.nextLine();
+//                System.out.println(taskCounter + " : " + task);
+//                taskCounter++;
+//            }
+//        } catch (IOException e){
+//            System.out.println("file not found");
+//        }
+
+    }
+
+    // METHOD ADDING TASK
+
+    public static void addTask(){
+
+
 
     }
 
